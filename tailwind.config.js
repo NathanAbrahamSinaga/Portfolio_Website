e473ca68870f47a3c751ebd00f582d5c7c1a7e9b
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-01-31 21:33:38 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html, js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors : {
+        primary: '#f97316',
+        secondary: '#64748b',
+        dark: '#9a3412',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
+
