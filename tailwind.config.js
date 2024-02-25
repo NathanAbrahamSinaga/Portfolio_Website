a0b064f32ae7aed62ac3d867706a6abefafef6b0
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-02-25 17:14:52 +0700

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html, js}'],
+  content: ["./Build/**/*.{html,js}"],
   theme: {
     container: {
       center: true,
